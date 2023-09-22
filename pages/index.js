fcb51d2c98476f6525b7c9da10fe146c668c08e6
jonas8900 <jonas.dally@hotmail.de>
2023-09-23 00:15:44 +0200

    adding introtext with extra window after click a button

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
+import Introtext from "../components/IntroText";
 import Navigation from "../components/Navigation";
 import Randompicture from "../components/Randompicturewithinfo";
+import SpeechBubbleHelp from "../components/SpeechBubbleHelp";
 
 export default function Home() {
   return (
     <>
       <Navigation />
+      <SpeechBubbleHelp />
       <Randompicture />
+      <Introtext />
     </>
   );
 }
